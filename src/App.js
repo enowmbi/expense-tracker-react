a73056423e04dc28ee34547a6d29952a895b13d6
Enow Mbi <benowmbi@yahoo.com>
2024-02-07 13:09:34 +0100

    Setup project with React 18

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header title="Expense Tracker" />
+    </div>
+  );
+}
+
+export default App;
